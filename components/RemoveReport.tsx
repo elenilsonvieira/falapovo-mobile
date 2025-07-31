060dd839bff424d863250228894a08716369fdca
jeacd <joao.euller@academico.ifpb.edu.br>
2025-07-31 16:53:52 -0300

    Create RemoveReport.tsx

---
+++ components/RemoveReport.tsx
@@ -0,0 +1,9 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { IReport } from "@/interfaces/IReport";
+
+export default async function RemoveReport(id: number, reports: IReport[], asyncKey: string) {
+    const newList = reports.filter((rep) => rep.id !== id);
+    await AsyncStorage.setItem(asyncKey, JSON.stringify(newList));
+    return newList;
+}
